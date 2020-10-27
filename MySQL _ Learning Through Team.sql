8eb75543f1192ab1f06957a43d03484872323268
CoryJacob <73498905+CoryJacob@users.noreply.github.com>
2020-10-26 17:41:10 -0700

    Update MySQL _ Learning Through Team.sql

--- MySQL _ Learning Through Team.sql
+++ MySQL _ Learning Through Team.sql
@@ -13,6 +13,7 @@
 -- Thus, it's important to make sure that each student in a group is teaching other students as well so that everyone in that group is getting highest possible score.
 -- See below for the questions and their answers.
 	
+    -- 12345
 
 Use sakila;
 
